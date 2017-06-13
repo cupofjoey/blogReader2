752bbda0aa0b288b5946e7218b6a285b0c0ba2e8
Joe Hill <joe@jkh3.com>
2017-06-13 00:15:43 -0400

    Google Blogger API Reader in Swift

---
+++ blogReader2/DetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DetailViewController.swift
+//  blogReader2
+//
+//  Created by BishopHill on 6/11/17.
+//  Copyright © 2017 Dumb Unicorn. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+   
+    @IBOutlet var webView: UIWebView!
+    
+    func configureView() {
+        // Update the user interface for the detail item.
+        
+        
+        if let detail = detailItem {
+            
+            self.title = detail.value(forKey: "title") as! String
+            
+            if let blogWebView = self.webView {
+                blogWebView.loadHTMLString((detail.value(forKey: "content") as! String), baseURL: nil)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
